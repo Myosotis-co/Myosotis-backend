ad590b7602d1ffde091dbe6ae868ed857377031d
test <test@test.copm>
2023-07-29 15:13:07 +0100

    changing database py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,17 @@
 
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}"
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}"
 
 Base = declarative_base()
 
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
