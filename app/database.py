18f098222a111bd3d5b2ec88810a65693d3e863c
test <test@test.copm>
2023-07-30 22:04:35 +0100

    changed to async

--- app/database.py
+++ app/database.py
@@ -12,6 +12,6 @@ engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-async def get_session() -> AsyncSession:
+async def get_async_session() -> AsyncSession:
     async with async_session() as session:
         yield session
