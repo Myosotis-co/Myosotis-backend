008b07f126ac827cfcdc35fbd724805f0c4aa16c
test <test@test.copm>
2023-07-30 22:02:16 +0100

    get user db for auth

---
+++ app/auth/functions.py
@@ -0,0 +1,12 @@
+
+
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from app.auth.manager import UserManager
+from app.auth.user import User
+from app.database import get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+
+#Get async db of the user
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session,User)
