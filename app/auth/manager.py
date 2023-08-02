673dee7b8f522485b506e9aa6cc454e30cd4a606
test <test@test.copm>
2023-08-02 22:04:19 +0100

    deleted userdict log

--- app/auth/manager.py
+++ app/auth/manager.py
@@ -36,7 +36,6 @@ class UserManager(IntegerIDMixin,BaseUserManager[User,int]):
         password = user_dict.pop("password")
         user_dict["hashed_password"] = password_helper.hash(password)
         user_dict["role_id"] = 2
-        print(user_dict)
         created_user = await self.user_db.create(user_dict)
 
         await self.on_after_register(created_user,request)
