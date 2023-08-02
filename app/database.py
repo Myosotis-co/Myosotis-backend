02e9406e514a93f30bbc31e58ea4893ccfae822e
test <test@test.copm>
2023-08-02 22:04:58 +0100

    Taking noolpool

--- app/database.py
+++ app/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
+from sqlalchemy.pool import NullPool
 
 SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}"
 
