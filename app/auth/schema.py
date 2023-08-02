c4f5753271b10b34b30ede44c8ce10f2f3edd280
test <test@test.copm>
2023-08-02 22:41:25 +0100

    Removed category from schema and added default role_id

--- app/auth/schema.py
+++ app/auth/schema.py
@@ -1,7 +1,10 @@
 
 import datetime
 from typing import List, Optional
+from typing import List, Optional
+from typing import List, Optional
 
+from app.schema import Category,UserBase
 from fastapi_users import schemas
 
 from app.schema import Category,UserBase
@@ -14,16 +17,14 @@ class UserRead(schemas.BaseUser[int]):
     is_deleted: bool = False
     is_active: bool = True
     is_verified: bool = False
-    categories: List[Category]
 
     class Config:
         orm_mode = True
 
-
 class UserCreate(schemas.BaseUserCreate):
     email: str
     password: str
-    role_id: int
+    role_id: int = 2
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
     is_verified: Optional[bool] = False
