e799768749773cd687f3eb7e946b4aaeb72800c3
Artur <zinnurov_2012@mail.ru>
2023-08-04 16:15:39 +0100

    Deleteing Base user table so we can use our own values

--- app/auth/models.py
+++ app/auth/models.py
@@ -16,7 +16,7 @@ class Role(Base):
     users = relationship("User", backref='users', passive_deletes=True)
 
 
-class User(SQLAlchemyBaseUserTable[int],Base):
+class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
@@ -26,8 +26,6 @@ class User(SQLAlchemyBaseUserTable[int],Base):
     user_token = Column(String, unique=True)
     hashed_password = Column(String)
     is_deleted = Column(Boolean, default=False)
-    is_active = Column(Boolean, default=True)
-    is_superuser = Column(Boolean, default=False)
     is_verified = Column(Boolean, default=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
     updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
