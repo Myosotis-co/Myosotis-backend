f136b0df3b32f956547dd0eb7bcc2d364884b00b
Artur <zinnurov_2012@mail.ru>
2023-08-06 23:04:49 +0100

    make router for seeder

---
+++ app/seeder/router.py
@@ -0,0 +1,22 @@
+
+
+from fastapi import APIRouter, Depends, HTTPException
+
+from app.database import get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.seeder.seeder import seed
+
+router = APIRouter()
+
+@router.post("/seeder")
+async def seeder_post(session: AsyncSession = Depends(get_async_session)):
+    try:
+        await seed(session)
+        return {
+            "status": 200,
+            "data": "Seed completed"
+        }
+    except Exception as e:
+        # Handle the exception and return an appropriate error response
+        raise HTTPException(status_code=500, detail=str(e))
