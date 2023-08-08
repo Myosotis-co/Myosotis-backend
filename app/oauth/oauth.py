26cce4aa1178597c607c7ede90b5b26e3c97caf5
Illia <aboba@mong.us>
2023-08-08 16:52:08 +0100

    Useless gone need new

--- app/oauth/oauth.py
+++ app/oauth/oauth.py
@@ -13,12 +13,15 @@ from httpx_oauth.clients.github import GitHubOAuth2
 from httpx_oauth.clients.linkedin import LinkedInOAuth2
 from httpx_oauth.clients.reddit import RedditOAuth2
 
+from app.database import SQLALCHEMY_DATABASE_URL
+
+
 reddit_oauth_client = RedditOAuth2("CLIENT_ID", "CLIENT_SECRET")
 linkedin_oauth_client = LinkedInOAuth2("CLIENT_ID", "CLIENT_SECRET")
 google_oauth_client = GoogleOAuth2("CLIENT_ID", "CLIENT_SECRET")
 github_oauth_client = GitHubOAuth2("CLIENT_ID", "CLIENT_SECRET")
 
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
 
 
 class Base(DeclarativeBase):
@@ -35,7 +38,7 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
     )
 
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
