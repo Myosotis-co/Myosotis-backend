2b74de588ad51f7512d8f552eeb0bdd434f44295
Arthur <test@test.copm>
2023-08-08 14:37:47 +0100

    Bunch of OAuth2 to sort

--- app/oauth/oauth.py
+++ app/oauth/oauth.py
@@ -1,10 +1,53 @@
-from httpx_oauth.integrations.fastapi import OAuth2AuthorizeCallback
-from httpx_oauth.oauth2 import OAuth2
+from typing import AsyncGenerator, List
 
-client = OAuth2("CLIENT_ID", "CLIENT_SECRET", "AUTHORIZE_ENDPOINT", "ACCESS_TOKEN_ENDPOINT")
-oauth2_authorize_callback = OAuth2AuthorizeCallback(client, "oauth-callback")
-app = FastAPI()
+from fastapi import Depends
+from fastapi_users.db import (
+    SQLAlchemyBaseOAuthAccountTableUUID,
+    SQLAlchemyBaseUserTableUUID,
+    SQLAlchemyUserDatabase,
+)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, relationship
+from httpx_oauth.clients.google import GoogleOAuth2
+from httpx_oauth.clients.github import GitHubOAuth2
+from httpx_oauth.clients.linkedin import LinkedInOAuth2
+from httpx_oauth.clients.reddit import RedditOAuth2
 
-@app.get("/oauth-callback", name="oauth-callback")
-async def oauth_callback(access_token_state=Depends(oauth2_authorize_callback)):
-    token, state = access_token_state
+reddit_oauth_client = RedditOAuth2("CLIENT_ID", "CLIENT_SECRET")
+linkedin_oauth_client = LinkedInOAuth2("CLIENT_ID", "CLIENT_SECRET")
+google_oauth_client = GoogleOAuth2("CLIENT_ID", "CLIENT_SECRET")
+github_oauth_client = GitHubOAuth2("CLIENT_ID", "CLIENT_SECRET")
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class OAuthAccount(SQLAlchemyBaseOAuthAccountTableUUID, Base):
+    pass
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    oauth_accounts: Mapped[List[OAuthAccount]] = relationship(
+        "OAuthAccount", lazy="joined"
+    )
+
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User, OAuthAccount)
