cb40a85d4d480e6a73624000ace0a371b8a2d18d
s0s100 <s0s100@mail.ru>
2023-08-30 17:09:52 +0300

    Fixed get_category
    It worked before, then suddenly stopped to work. Why?!

--- app/category/router.py
+++ app/category/router.py
@@ -1,49 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException
-
 from app.database import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, delete
+from sqlalchemy import select
 
 from app.category.models import Category
+from app.schema import Category as Schema_category
+from app.category.functions import *
 
 router = APIRouter(tags=["Category"])
-
-
-@router.get("/categories/get/{category_id}")
-async def get_category(
-    category_id: int, session: AsyncSession = Depends(get_async_session)
-):
-    exec_command = select(Category).filter(Category.id == category_id)
-    result_value = await session.execute(exec_command)
-    category = result_value.scalar()
-
-    return category
-
-
-@router.put("/categories/update/{category_id}")
-async def update_category(
-    category_id: int, session: AsyncSession = Depends(get_async_session)
-):
-    return {"status": 204, "data": "Category is updated"}
-    # exec_command = select(Category).filter(Category.id == category_id)
-    # result_value = await session.execute(exec_command)
-    # category = result_value.scalar()
-
-    # await session.commit()
-
-
-# Relocate this function to function.py
-def add_category(
-    user_id: int,
-    temp_email_id: int,
-    category_name: str,
-    session: AsyncSession = Depends(get_async_session),
-):
-    new_category = Category(
-        user_id=user_id, temp_email_id=temp_email_id, category_name=category_name
-    )
-    session.add(new_category)
-    return new_category
 
 
 @router.post("/categories/create")
@@ -53,42 +17,73 @@ async def create_category(
     category_name: str,
     session: AsyncSession = Depends(get_async_session),
 ):
-    new_category = add_category(user_id, temp_email_id, category_name, session)
+    new_category = service_add_category(user_id, temp_email_id, category_name, session)
     try:
         await session.commit()
+        # return {"status": 201, "data": "Category is created"}
         return new_category
     except Exception as e:
         return "Failed to create new category: {e}"
-    # except IntegrityError as ex:
-    #     await session.rollback()
-    #     raise DuplicatedEntryError("Such a category already exist!")
 
 
-@router.delete("/categories/delete/{category_id}")
-async def delete_category(
+@router.get("/categories/get/{category_id}")
+async def get_category(
     category_id: int, session: AsyncSession = Depends(get_async_session)
 ):
-    # Try #4
-    # session.query()
+    try:
+        category = await service_get_category(category_id, session)
+        return category
+    except Exception as e:
+        return "Failed to get a category: {e}"
 
-    # Try #3
-    # exec_command = select(Category).filter(Category.id == category_id)
-    # result_value = await session.execute(exec_command)
-    # category = result_value.scalar()
 
-    # await session.delete(category)
+# To implement
+# @router.get("/categories/get/list", response_model=list[Schema_category])
+# async def get_categories(session: AsyncSession = Depends(get_async_session)):
+#     # categories = await service_get_categories(session)
+#     # return [Schema_category(id = category.id) for category in categories]
+#     return "Cool!"
 
-    # Tries #1 and #2
-    # sql_query = "DELETE FROM categories WHERE categories.id = " + str(
-    #     category_id
-    # )  # Not working :(
 
-    # exec_command = delete(Category).where(Category.id == category_id)
-    # sql_query = str(exec_command)
+# To implement
+# @router.put("/categories/update/{category_id}")
+# async def update_category(
+#     category_id: int, session: AsyncSession = Depends(get_async_session)
+# ):
+#     return {"status": 204, "data": "Category is updated"}
+#     # exec_command = select(Category).filter(Category.id == category_id)
+#     # result_value = await session.execute(exec_command)
+#     # category = result_value.scalar()
 
-    # session.delete()
+#     # await session.commit()
 
-    # print(sql_query)
-    # await session.execute(sql_query)
 
-    return {"status": 200, "data": "Category is deleted"}
+# To implement
+# @router.delete("/categories/delete/{category_id}")
+# async def delete_category(
+#     category_id: int, session: AsyncSession = Depends(get_async_session)
+# ):
+#     # Try #4
+#     # session.query()
+
+#     # Try #3
+#     # exec_command = select(Category).filter(Category.id == category_id)
+#     # result_value = await session.execute(exec_command)
+#     # category = result_value.scalar()
+
+#     # await session.delete(category)
+
+#     # Tries #1 and #2
+#     # sql_query = "DELETE FROM categories WHERE categories.id = " + str(
+#     #     category_id
+#     # )  # Not working :(
+
+#     # exec_command = delete(Category).where(Category.id == category_id)
+#     # sql_query = str(exec_command)
+
+#     # session.delete()
+
+#     # print(sql_query)
+#     # await session.execute(sql_query)
+
+#     return {"status": 200, "data": "Category is deleted"}
