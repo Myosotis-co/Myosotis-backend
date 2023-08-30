9b4832f5f4b3936f770c77f8fb1afd9d763b7051
s0s100 <s0s100@mail.ru>
2023-08-30 13:49:53 +0300

    Create category route

--- app/category/router.py
+++ app/category/router.py
@@ -2,14 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from app.database import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, delete
 
 from app.category.models import Category
 
 router = APIRouter(tags=["Category"])
 
 
-@router.get("/categories/{category_id}")
+@router.get("/categories/get/{category_id}")
 async def get_category(
     category_id: int, session: AsyncSession = Depends(get_async_session)
 ):
@@ -17,26 +17,78 @@ async def get_category(
     result_value = await session.execute(exec_command)
     category = result_value.scalar()
 
-    return {category}
+    return category
 
 
-# @router.post("/create_category", response_model=Category)
-# async def seeder_post(
-#     post: CategoryCreate, session: AsyncSession = Depends(get_async_session)
-# ):
-#     try:
-#         await create_category(post, session)
-#         return {"status": 201, "data": "Category created"}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+@router.put("/categories/update/{category_id}")
+async def update_category(
+    category_id: int, session: AsyncSession = Depends(get_async_session)
+):
+    return {"status": 204, "data": "Category is updated"}
+    # exec_command = select(Category).filter(Category.id == category_id)
+    # result_value = await session.execute(exec_command)
+    # category = result_value.scalar()
+
+    # await session.commit()
 
 
-# @router.post("/categories")
-# async def create_category(
-#     category: Category, session: AsyncSession = Depends(get_async_session)
-# ):
-#     try:
-#         await category_creation(category, session)
-#         return {"status": 200, "data": "Category created"}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+# Relocate this function to function.py
+def add_category(
+    user_id: int,
+    temp_email_id: int,
+    category_name: str,
+    session: AsyncSession = Depends(get_async_session),
+):
+    new_category = Category(
+        user_id=user_id, temp_email_id=temp_email_id, category_name=category_name
+    )
+    session.add(new_category)
+    return new_category
+
+
+@router.post("/categories/create")
+async def create_category(
+    user_id: int,
+    temp_email_id: int,
+    category_name: str,
+    session: AsyncSession = Depends(get_async_session),
+):
+    new_category = add_category(user_id, temp_email_id, category_name, session)
+    try:
+        await session.commit()
+        return new_category
+    except Exception as e:
+        return "Failed to create new category: {e}"
+    # except IntegrityError as ex:
+    #     await session.rollback()
+    #     raise DuplicatedEntryError("Such a category already exist!")
+
+
+@router.delete("/categories/delete/{category_id}")
+async def delete_category(
+    category_id: int, session: AsyncSession = Depends(get_async_session)
+):
+    # Try #4
+    # session.query()
+
+    # Try #3
+    # exec_command = select(Category).filter(Category.id == category_id)
+    # result_value = await session.execute(exec_command)
+    # category = result_value.scalar()
+
+    # await session.delete(category)
+
+    # Tries #1 and #2
+    # sql_query = "DELETE FROM categories WHERE categories.id = " + str(
+    #     category_id
+    # )  # Not working :(
+
+    # exec_command = delete(Category).where(Category.id == category_id)
+    # sql_query = str(exec_command)
+
+    # session.delete()
+
+    # print(sql_query)
+    # await session.execute(sql_query)
+
+    return {"status": 200, "data": "Category is deleted"}
