9c5886e0b06af35718d3027201e3f7220642841e
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2023-10-01 19:58:00 +0100

    Add service_add_temp_email to email/functions

--- app/email/functions.py
+++ app/email/functions.py
@@ -1,6 +1,13 @@
 import random
 import string
 
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import get_async_session
+from app.email.models import Temp_Email
+from app.email.schema import Temp_Email
+
 
 def generate_random_mailsac_email():
     random_prefix = "".join(
@@ -8,3 +15,15 @@ def generate_random_mailsac_email():
     )
     mailsac_temp_email = random_prefix + "@mailsac.com"
     return mailsac_temp_email
+
+
+def service_add_temp_email(
+    id: int,
+    email: str,
+    access_token: str,
+    session: AsyncSession = Depends(get_async_session),
+):
+    new_temp_email = Temp_Email(id=id, email=email, access_token=access_token)
+    session.add(new_temp_email)
+    session.commit()
+    return new_temp_email
