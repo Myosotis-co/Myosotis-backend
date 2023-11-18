ba327ef60fa3b248f6d6bda3009fa1bc7ace061d
Arthur <zinnurov_2012@mail.ru>
2023-11-18 22:19:49 +0000

    removing failed test

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,46 +10,13 @@ from httpx import AsyncClient
 import pytest
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from tests.confest import async_db, async_db_engine, async_client
+from tests.confest import async_db, async_db_engine, async_client,generate_email
 
 
 pytestmark = pytest.mark.asyncio
 
 
-async def test_create_category(async_client: AsyncClient, async_db: AsyncSession):
-    temp_email_create = TempEmail_model(email='test@gmail.com')
-    user_create = UserCreate(
-        name="test",
-        email=temp_email_create.email,
-        password="test",
-    )
-    user =  service_create_model(User, user_create, async_db)
 
-    temp_email_create.user_id = 1
-    service_add_model(temp_email_create, async_db)
+async def test_email(async_client: AsyncClient, async_db: AsyncSession,generate_email:TempEmail_model):
+    assert generate_email.email == 'test@gmail.com'
 
-    category_create = CategoryCreate(
-        category_name="Test d",
-        temp_email_id=1,
-        user_id=1
-    )
-    response = await async_client
-    response = await response.post(
-        "/category/categories/create", json=category_create.dict()
-    )
-    assert response.status_code == 200
-    assert response.json() == {"status": 200, "data": "Category is created"}
-
-    async with await async_db as session:
-        
-        category = await service_get_model(
-            Category_model, category_create.name, session
-        )
-        assert category is not None
-
-async def test_add_email(async_db: AsyncSession,async_client: AsyncClient,):
-    email = TempEmail_model(email='test@gmail.com')
-    async_db.add(email)
-    await async_db.commit()
-    await async_db.refresh(email)
-    assert email.id == 1
