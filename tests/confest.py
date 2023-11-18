63b6f09954c86483575db7d246365fe8170e5027
Arthur <zinnurov_2012@mail.ru>
2023-11-18 22:19:29 +0000

    adding confest file

---
+++ tests/confest.py
@@ -0,0 +1,64 @@
+import asyncio
+from app.email.models import TempEmail
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from app.config import settings
+from app.database import Base, engine
+import pytest
+from sqlalchemy.orm import sessionmaker
+from app.main import app
+from httpx import AsyncClient
+
+
+engine = create_async_engine(
+    f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}",
+    echo=True,
+)
+
+
+
+@pytest.fixture
+async def async_db_engine():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield engine
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="function")
+async def async_db(async_db_engine):
+    async_session = sessionmaker(
+        expire_on_commit=False,
+        autocommit=False,
+        autoflush=False,
+        bind=async_db_engine,
+        class_=AsyncSession,
+    )
+
+    async with async_session() as session:
+        await session.begin()
+
+        yield session
+
+        await session.rollback()
+
+        for table in reversed(Base.metadata.sorted_tables):
+            await session.execute(f'TRUNCATE {table.name} CASCADE;')
+            await session.commit()
+
+
+@pytest.fixture
+async def async_client() -> AsyncClient:
+    return AsyncClient(app=app, base_url="http://localhost")
+
+
+
+@pytest.fixture
+async def generate_email(async_db:AsyncSession):
+    email = TempEmail(email='test@gmail.com')
+    async_db.add(email)
+    await async_db.commit()
+    await async_db.refresh(email)
+    return email
