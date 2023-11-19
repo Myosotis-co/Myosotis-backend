353148243fc9a6e271aa21d0fc8e22f55bab53ee
Arthur zinnurov <zinnurov_2012@mail.ru>
2023-11-19 12:45:10 +0000

    some black fixes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,3 @@
-
 import pytest
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,7 +7,5 @@ from app.email.models import TempEmail as TempEmail_model
 pytestmark = pytest.mark.asyncio
 
 
-async def test_email(
-    async_client: AsyncClient, async_db: AsyncSession, generate_email: TempEmail_model
-):
+async def test_email(generate_email: TempEmail_model):
     assert generate_email.email == "test@gmail.com"
