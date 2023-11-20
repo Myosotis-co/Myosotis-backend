30c38bb977f09063430d58554a5d1e4b543f4b99
Arthur zinnurov <zinnurov_2012@mail.ru>
2023-11-20 12:55:56 +0000

    adding test fail and not fail

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,8 +13,13 @@ async def test_email(generate_email: TempEmail_model):
 
 
 def test_fail():
-    assert 1 == 2
+    assert 1 == 1
 
+def test_fail():
+    assert 1 == '1'
 
 def test_good():
     assert 1 == 1
+
+def test_loh():
+    assert "loh" == "loh"
