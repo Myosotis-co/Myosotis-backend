ba344f9f170746477ec4bcb3f135647a2272a004
Arthur zinnurov <zinnurov_2012@mail.ru>
2024-09-04 09:58:11 +0100

    application fix if user is not allowed to access

--- app/application/router.py
+++ app/application/router.py
@@ -1,14 +1,16 @@
+from app.auth.models import User
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_async_session
 from app.application.schema import *
 from app.application.models import Application as Application_model
+from app.category.models import Category as Category_model
 from app.crud_manager import *
 from app.auth.jwt_config import fastapi_users
 
 current_user = fastapi_users.current_user(active=True)
-router = APIRouter(tags=["Application"],dependencies=[Depends(current_user)])
+router = APIRouter(tags=["Application"], dependencies=[Depends(current_user)])
 
 
 @router.post("/create")
@@ -26,15 +28,22 @@ async def create_application(
 
 @router.get("/get/{category_id}")
 async def get_application(
-    application_id: int, session: AsyncSession = Depends(get_async_session)
+    application_id: int,
+    session: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_user),
 ):
     try:
         application = await service_get_model(
             Application_model, application_id, session
         )
         if application is not None:
+            category = await service_get_model(
+                Category_model, application.category_id, session
+            )
+            if category.user_id != user.id:
+                return HTTPException(status_code=403, detail="Forbidden")
             return application
-        raise HTTPException(status_code=404, detail="Applicaion not found")
+        return HTTPException(status_code=404, detail="Applicaion not found")
     except Exception as e:
         return "Failed to get an applicaion: " + str(e)
 
@@ -44,28 +53,46 @@ async def update_application(
     application_id: int,
     application_update: ApplicationUpdate,
     session: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_user),
 ):
     try:
         application = await service_get_model(
             Application_model, application_id, session
         )
         if application is not None:
+            category = await service_get_model(
+                Category_model, application.category_id, session
+            )
+            if category.user_id != user.id:
+                return HTTPException(status_code=403, detail="Forbidden")
             await service_update_model(application, application_update, session)
             await session.commit()
             return {"status": 204, "data": "Application is updated"}
-        raise HTTPException(status_code=404, detail="Application not found")
+        return HTTPException(status_code=404, detail="Application not found")
     except Exception as e:
         return "Failed to update an application: " + str(e)
 
 
 @router.delete("/delete/{application_id}")
 async def delete_application(
-    application_id: int, session: AsyncSession = Depends(get_async_session)
+    application_id: int,
+    session: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_user),
 ):
     try:
-        await service_delete_model(Application_model, application_id, session)
-        await session.commit()
-        return {"status": 204, "data": "Application is deleted"}
+        application = await service_get_model(
+            Application_model, application_id, session
+        )
+        category = await service_get_model(
+            Category_model, application.category_id, session
+        )
+        if application is not None:
+            if category.user_id != user.id:
+                return HTTPException(status_code=403, detail="Forbidden")
+            await service_delete_model(Application_model, application_id, session)
+            await session.commit()
+            return {"status": 204, "data": "Application is deleted"}
+        return HTTPException(status_code=404, detail="Application not found")
     except Exception as e:
         return "Failed to delete an application: " + str(e)
 
@@ -75,11 +102,21 @@ async def get_applications(
     page_num: int,
     items_per_page: int,
     session: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_user),
 ):
     try:
         applications = await service_get_some_models(
             Application_model, page_num, items_per_page, session
         )
-        return applications
+        applications = [application for application in applications if application is not None]
+        validatated_applications = []
+        for application in applications:
+            category = await service_get_model(
+                Category_model, application.Application.category_id, session
+            )
+            if category.user_id == user.id:
+                validatated_applications.append(application)
+
+        return validatated_applications
     except Exception as e:
         return "Failed to get applications: " + str(e)
