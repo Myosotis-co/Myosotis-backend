4d842eddda4de59f7d9b66c5a8fbd5c019f464dd
Arthur zinnurov <zinnurov_2012@mail.ru>
2025-03-16 19:19:06 +0000

    adding router calls for reading emails

--- app/gmail_api/router.py
+++ app/gmail_api/router.py
@@ -1,6 +1,11 @@
 from urllib.parse import unquote
 from fastapi import APIRouter, Request
-from app.gmail_api.functions import get_google_access
+from app.gmail_api.api_access_utils import (
+    create_credentials_object,
+    get_credential_metadata,
+    get_google_access,
+)
+from app.gmail_api.functions import gmail_read_messages
 
 
 router = APIRouter(tags=["DevelopeOnly"])
@@ -9,12 +14,22 @@ router = APIRouter(tags=["DevelopeOnly"])
 @router.get("/google_access")
 def google_request_api(request: Request, code: str = None):
     try:
-        if not code:
-            return get_google_access()
-        else:
-            response = get_google_access(unquote(code))
-            request.app.state.google_token = get_google_access(unquote(code))
-            return {"status": 200, "data": response}
+        response = get_google_access(unquote(code)) if code else get_google_access()
+        request.app.state.google_token = create_credentials_object(
+            get_credential_metadata(), response["access_token"]
+        )
+
+        return {"status": 200, "data": response}
 
     except Exception as e:
         return "Failed to get a token: " + str(e)
+
+
+@router.get("/gmail_read_message")
+def google_read_message(request: Request):
+    try:
+        creds = request.app.state.google_token
+        print(creds)
+        gmail_read_messages(creds)
+    except Exception as e:
+        return "Failed to get a message" + str(e)
