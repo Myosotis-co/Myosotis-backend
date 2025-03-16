10310f9d285d30714ff41d4bacedaefcf74d1057
Arthur zinnurov <zinnurov_2012@mail.ru>
2025-03-16 19:16:43 +0000

    adding initial gmail message read

--- app/gmail_api/functions.py
+++ app/gmail_api/functions.py
@@ -1,41 +1,18 @@
-import json
-import requests
-import os
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+from googleapiclient.discovery import build
 
 
-def get_google_access(code: str = None):
-    if not code:
-        if os.path.exists("token.json"):
-            with open("token.json", "r") as token:
-                return json.load(token)
-        else:
-            auth_data = get_credential_metadata()
-            auth_uri = f"{auth_data['auth_uri']}?response_type=code&client_id={auth_data['client_id']}&redirect_uri=http://localhost:8000&scope={SCOPES[0]}"
-            return auth_uri
-    else:
-        metadata = get_credential_metadata()
-        token_request = {
-            "code": code,
-            "client_id": metadata["client_id"],
-            "client_secret": metadata["client_secret"],
-            "redirect_uri": "http://localhost:8000",
-            "grant_type": "authorization_code",
-        }
-        try:
-            r = requests.post("https://oauth2.googleapis.com/token", data=token_request)
-            with open("token.json", "w") as token:
-                token.write(r.text)
-                token.close()
-            return r.json()
-        except Exception as e:
-            return {"error": str(e)}
-
-
-def get_credential_metadata():
-    with open("app/gmail_api/credentials.json", "r") as f:
-        auth_data = json.load(f)["web"]
-        f.close()
-        return auth_data
+def gmail_read_messages(creds: str = None):
+    try:
+        service = build("gmail", "v1", credentials=creds)
+        results = (
+            service.users()
+            .messages()
+            .list(userId="me", labelIds=["INBOX"], q="is:unread")
+            .execute()
+        )
+        messages = results.get("messages", [])
+        print(messages)
+        if not messages:
+            print("No new messages.")
+    except Exception as e:
+        print(f"Error occured:{e}")
