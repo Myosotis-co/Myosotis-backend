109e865aeb793524e97ea2adbd367f694598635d
Arthur zinnurov <zinnurov_2012@mail.ru>
2025-03-16 17:24:27 +0000

    adding router changes

--- app/gmail_api/router.py
+++ app/gmail_api/router.py
@@ -1,5 +1,5 @@
+from urllib.parse import unquote
 from fastapi import APIRouter, Request
-import app
 from app.gmail_api.functions import get_google_access
 
 
@@ -7,9 +7,14 @@ router = APIRouter(tags=["DevelopeOnly"])
 
 
 @router.get("/google_access")
-def apiaccessRequest(code: str = None):
+def google_request_api(request: Request, code: str = None):
     try:
-        app.state.google_token = get_google_access(code)
+        if not code:
+            return get_google_access()
+        else:
+            response = get_google_access(unquote(code))
+            request.app.state.google_token = get_google_access(unquote(code))
+            return {"status": 200, "data": response}
 
     except Exception as e:
         return "Failed to get a token: " + str(e)
